00a9a701c5127322843cc403cf96d39af1e7042d
grafismoweb <grafismoweb@gmail.com>
2021-09-27 14:40:54 +0200

    vista, plantilla y estilos less

--- app/code/Hiberus/Rodriguez/view/frontend/web/js/alert.js
+++ app/code/Hiberus/Rodriguez/view/frontend/web/js/alert.js
@@ -3,7 +3,7 @@ define(['jquery'], function($){
     return function alertNotaAlta(btn)
     {
         $(btn).click(function(){
-            alert('La nota mayor de la clase es: ' + maxNote);
+            alert('La mayor nota del curso fue un ' + maxNote);
         })
     }
 });
